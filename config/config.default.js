5558db685fd5f7cde7123f8d87ada251515e6891
Burt <2902978956@qq.com>
2021-04-26 20:55:19 +0800

    跨域配置修改

--- config/config.default.js
+++ config/config.default.js
@@ -35,10 +35,9 @@ module.exports = appInfo => {
       enable: false,
       ignoreJSON: true
     },
-    // domainWhiteList: ['http://www.baidu.com', 'http://localhost:8080'], //配置白名单
+    domainWhiteList: ['http://localhost:8080', 'http://orange_ice.gitee.io/online-blog', 'https://orange_ice.gitee.io/online-blog'], //配置白名单
   }
   config.cors = {
-    origin: 'http://orange_ice.gitee.io/online-blog', //允许所有跨域访问，注释掉则允许上面 白名单 访问
     credentials: true, // 允许跨域请求携带cookies
     allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
   };
